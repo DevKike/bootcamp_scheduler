eacfed4aab845fd70793e9943b685e8c3b0f4cdd
DevKike <alvaronarvaez151@gmail.com>
2024-03-19 20:45:51 -0500

    Initial files were created

---
+++ src/test/java/com/technologies/microservice/TechnologiesMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.technologies.microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnologiesMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
