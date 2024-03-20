dd7fa3968e0fbbe4d8187a9637783b055b96008a
DevKike <alvaronarvaez151@gmail.com>
2024-03-19 21:47:23 -0500

    Save technologies logic was added

--- src/main/java/com/technologies/microservice/domain/api/ITechnologyServicePort.java
+++ src/main/java/com/technologies/microservice/domain/api/ITechnologyServicePort.java
@@ -1,7 +1,8 @@
 package com.technologies.microservice.domain.api;
 
-
+import com.technologies.microservice.domain.model.Technology;
+import org.springframework.web.bind.annotation.RequestParam;
 
 public interface ITechnologyServicePort {
-
+    void saveTechnology(Technology technology);
 }
