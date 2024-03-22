7611d9e2f67b6bb3979a4ac7fee903128b77b995
Álvaro Narváez <99517307+DevKike@users.noreply.github.com>
2024-03-22 17:11:10 -0500

    Revert "PR from feature/register_technologies_logic to develop"

--- src/main/java/com/bootcamp/scheduler/configuration/Constants.java
+++
@@ -1,9 +0,0 @@
-package com.bootcamp.scheduler.configuration;
-
-public class Constants {
-    private Constants() {
-        throw new IllegalStateException("Utility class");
-    }
-
-    public static final String EMPTY_FIELD_EXCEPTION_MESSAGE = "Field %s cannot be empty";
-}
--- src/main/java/com/bootcamp/scheduler/domain/exception/MaxSizeExceededException.java
+++
@@ -1,7 +0,0 @@
-package com.bootcamp.scheduler.domain.exception;
-
-public class MaxSizeExceededException extends RuntimeException {
-    public MaxSizeExceededException(String field, int maxLength) {
-        super(String.format("%s cannot exceed %d characters", field, maxLength));
-    }
-}
