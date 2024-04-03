be4087723dea466aebf6e61d536e107770743fc6
DevKike <alvaronarvaez151@gmail.com>
2024-04-03 16:29:46 -0500

    Capacity use case was fixed

--- src/main/java/com/bootcamp/scheduler/domain/api/usecase/CapacityUseCase.java
+++ src/main/java/com/bootcamp/scheduler/domain/api/usecase/CapacityUseCase.java
@@ -2,16 +2,17 @@ package com.bootcamp.scheduler.domain.api.usecase;
 
 import com.bootcamp.scheduler.domain.api.ICapacityServicePort;
 import com.bootcamp.scheduler.domain.model.Capacity;
+import com.bootcamp.scheduler.domain.spi.ICapacityPersistencePort;
 
 public class CapacityUseCase implements ICapacityServicePort {
-    private final ICapacityServicePort capacityServicePort;
+    private final ICapacityPersistencePort capacityPersistencePort;
 
-    public CapacityUseCase(ICapacityServicePort capacityServicePort) {
-        this.capacityServicePort = capacityServicePort;
+    public CapacityUseCase(ICapacityPersistencePort capacityPersistencePort) {
+        this.capacityPersistencePort = capacityPersistencePort;
     }
 
     @Override
     public void addCapacity(Capacity capacity) {
-        capacityServicePort.addCapacity(capacity);
+        capacityPersistencePort.addCapacity(capacity);
     }
 }
