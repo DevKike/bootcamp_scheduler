59ebe5dfe3b433cbb301bd988f71faca07fbdbca
DevKike <alvaronarvaez151@gmail.com>
2024-04-05 22:49:21 -0500

    Exception's name was changed

--- src/main/java/com/bootcamp/scheduler/domain/exception/MaxSizeExceededException.java
+++
@@ -1,7 +0,0 @@
-package com.bootcamp.scheduler.domain.exception;
-
-public class MaxSizeExceededException extends RuntimeException {
-    public MaxSizeExceededException(String message) {
-        super(message);
-    }
-}
