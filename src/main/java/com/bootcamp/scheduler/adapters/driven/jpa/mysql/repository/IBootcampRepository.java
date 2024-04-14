deaef121adac9afd3cc0d6d29d22540cab9ec6c7
DevKike <alvaronarvaez151@gmail.com>
2024-04-14 16:57:41 -0500

    Added an Optional for findById

--- src/main/java/com/bootcamp/scheduler/adapters/driven/jpa/mysql/repository/IBootcampRepository.java
+++ src/main/java/com/bootcamp/scheduler/adapters/driven/jpa/mysql/repository/IBootcampRepository.java
@@ -7,4 +7,5 @@ import java.util.Optional;
 
 public interface IBootcampRepository extends JpaRepository<BootcampEntity, Long> {
     Optional<BootcampEntity> findByName(String name);
+    Optional<BootcampEntity> findById(Long id);
 }
