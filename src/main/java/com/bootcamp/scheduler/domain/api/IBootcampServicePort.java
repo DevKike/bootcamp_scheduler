dbf0816429effac21bfafa5701014491ab5ec01d
DevKike <alvaronarvaez151@gmail.com>
2024-04-14 16:56:51 -0500

    Associate capacities with bootcamp logic for domain was added

--- src/main/java/com/bootcamp/scheduler/domain/api/IBootcampServicePort.java
+++ src/main/java/com/bootcamp/scheduler/domain/api/IBootcampServicePort.java
@@ -2,6 +2,9 @@ package com.bootcamp.scheduler.domain.api;
 
 import com.bootcamp.scheduler.domain.model.Bootcamp;
 
+import java.util.Set;
+
 public interface IBootcampServicePort {
     void addBootcamp(Bootcamp bootcamp);
+    void associateCapacitiesWithBootcamp(Long bootcampId, Set<Long> capacitiesIds);
 }
--- src/main/java/com/bootcamp/scheduler/domain/spi/IBootcampPersistencePort.java
+++ src/main/java/com/bootcamp/scheduler/domain/spi/IBootcampPersistencePort.java
@@ -2,6 +2,9 @@ package com.bootcamp.scheduler.domain.spi;
 
 import com.bootcamp.scheduler.domain.model.Bootcamp;
 
+import java.util.Set;
+
 public interface IBootcampPersistencePort {
     void addBootcamp(Bootcamp bootcamp);
+    void associateCapacitiesWithBootcamp(Long bootcampId, Set<Long> capacitiesIds);
 }
