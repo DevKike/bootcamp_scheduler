ff2f5c9b1d5941e182c0c1f8becbe71a6ebd0f33
DevKike <alvaronarvaez151@gmail.com>
2024-04-23 01:36:48 -0500

    Worked on list bootcamps logic

---
+++ src/main/java/com/bootcamp/scheduler/adapters/driving/http/dto/response/BootcampResponse.java
@@ -0,0 +1,15 @@
+package com.bootcamp.scheduler.adapters.driving.http.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.List;
+
+@AllArgsConstructor
+@Getter
+public class BootcampResponse {
+    private final Long id;
+    private final String name;
+    private final String description;
+    private List<CapacityWithoutDescriptionResponse> capacities;
+}
