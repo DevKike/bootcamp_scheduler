faf74b34cf6bc9bad9c1f5bf6edb82e34a0cac38
DevKike <alvaronarvaez151@gmail.com>
2024-04-23 21:45:52 -0500

    Worked on list versions logic

--- src/main/java/com/bootcamp/scheduler/adapters/driving/http/controller/VersionRestControllerAdapter.java
+++ src/main/java/com/bootcamp/scheduler/adapters/driving/http/controller/VersionRestControllerAdapter.java
@@ -1,12 +1,16 @@
 package com.bootcamp.scheduler.adapters.driving.http.controller;
 
 import com.bootcamp.scheduler.adapters.driving.http.dto.request.AddVersionRequest;
+import com.bootcamp.scheduler.adapters.driving.http.dto.response.VersionResponse;
 import com.bootcamp.scheduler.adapters.driving.http.mapper.IVersionRequestMapper;
+import com.bootcamp.scheduler.adapters.driving.http.mapper.IVersionResponseMapper;
 import com.bootcamp.scheduler.domain.api.IVersionServicePort;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/version")
@@ -14,10 +18,16 @@ import org.springframework.web.bind.annotation.*;
 public class VersionRestControllerAdapter {
     private final IVersionServicePort versionServicePort;
     private final IVersionRequestMapper versionRequestMapper;
+    private final IVersionResponseMapper versionResponseMapper;
 
     @PostMapping("/add")
     public ResponseEntity<Void> addVersion(@RequestBody AddVersionRequest request) {
         versionServicePort.addVersion(versionRequestMapper.addRequestToVersion(request));
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @GetMapping("/getAll")
+    public ResponseEntity<List<VersionResponse>> getAllVersions(@RequestParam Integer page, Integer size, boolean isAscending) {
+        return ResponseEntity.ok(versionResponseMapper.toVersionResponseList(versionServicePort.getAllVersions(page, size, isAscending)));
     }
 }
