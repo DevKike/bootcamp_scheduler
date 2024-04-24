4d1797cf6b03fadf6d5c51bcff94211afab0e1bc
DevKike <alvaronarvaez151@gmail.com>
2024-04-23 21:43:55 -0500

    To model list mapper was added

--- src/main/java/com/bootcamp/scheduler/adapters/driven/jpa/mysql/mapper/IVersionEntityMapper.java
+++ src/main/java/com/bootcamp/scheduler/adapters/driven/jpa/mysql/mapper/IVersionEntityMapper.java
@@ -5,8 +5,12 @@ import com.bootcamp.scheduler.domain.model.Version;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface IVersionEntityMapper {
     @Mapping(target = "bootcamp", source = "bootcamp")
     VersionEntity toEntity(Version version);
+
+    List<Version> toModelList(List<VersionEntity> versionEntities);
 }
